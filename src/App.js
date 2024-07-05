846c46858d5dbbc506a0a826ace32ef13427b740
bipulpr226 <pradhanbipul125@gmail.com>
2024-07-05 11:29:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.sdfsfsfvddfv
+          Learn React.js
         </a>
       </header>
     </div>
