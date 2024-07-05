ce1bfdc47ec2b6e0f395244e07be86638916f67b
bipulpr226 <pradhanbipul125@gmail.com>
2024-07-05 22:47:38 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
-import Footer from './components/Footer';
- const App = () => {
-return (
-  <>
-   <Header/>
+import Logo from "./assets/images/resturant-removebg-preview.png"
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+import { RouterProvider } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <>
+   <Header/>;
 {/* <Ro   uterProvider router={appRouter}/> */}
-<Body/>
- <Footer/>
+<Outlet/>
+   <Footer/> 
    </>
 
-)
-
-
- }
+  );
+}
 
 export default App;
